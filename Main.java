7f783f652b89c941e5c96de37c47ea0ee8933a45
Gabriel De Souza Passos <44300759+EastBeng@users.noreply.github.com>
2021-04-25 15:05:55 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    TBloco bloco = new TBloco();//Instancia a Classe TBloco
-
-    System.out.println(bloco);//Mostra a ultima configuração do bloco
-    
-  }
-}
